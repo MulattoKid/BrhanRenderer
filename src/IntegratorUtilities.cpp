b362ad4193b0fcb1620af243e45e33945bbcea51
Daniel Fedai Larsen <danielfedailarsen@hotmail.com>
2018-08-24 16:05:42 +0200

    IntegratorUtilities TODO

---
+++ src/IntegratorUtilities.cpp
@@ -0,0 +1,16 @@
+#include "IntegratorUtilities.h"
+
+void EstimateDirect()
+{
+	//TODO
+}
+
+void UniformSampleOne()
+{
+	//TODO
+}
+
+void UniformSampleAll()
+{
+	//TODO
+}
---
+++ src/IntegratorUtilities.h
@@ -0,0 +1,8 @@
+#ifndef INTEGRATORUTILITIES_H
+#define INTEGRATORUTILITIES_H
+
+void EstimateDirect();
+void UniformSampleOne();
+void UniformSampleAll();
+
+#endif
