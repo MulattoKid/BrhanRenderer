e0c8268d493f2bf57a43213725aa2096ccdd931e
Daniel Fedai Larsen <danielfedailarsen@hotmail.com>
2018-09-07 09:58:24 +0200

    Fix direct lighting
    Fix intersection testing during light sampling
    Fix incorrect wo direction in light sampling and BSDF sampling
      isect.ray->dir is now -isect.ray->dir

--- src/IntegratorUtilities.cpp
+++ src/IntegratorUtilities.cpp
@@ -1,6 +1,7 @@
 #include "BSDF.h"
 #include "glm/geometric.hpp"
 #include "IntegratorUtilities.h"
+#include <math.h>
 #include "Logger.h"
 #include "RNG.h"
 
@@ -31,34 +32,27 @@ glm::vec3 EstimateDirect(const Scene& scene, RNG& rng, const AreaLight* area_lig
 	glm::vec3 sample_point(0.0f), wi(0.0f);
 	float light_pdf = 0.0f;
 	glm::vec3 Li = area_light->SampleLi(rng, isect, u_light, &sample_point, &wi, &light_pdf);
-	//LOG_MESSAGE(true, "AreaLight: %p\n", area_light);
-	//LOG_MESSAGE(true, "SamplePoint: %f %f %f\n", sample_point.x, sample_point.y, sample_point.z);
-	//LOG_MESSAGE(true, "Wi: %f %f %f\n", wi.x, wi.y, wi.z);
-	if (light_pdf > 0.0f && Li != glm::vec3(0.0f)) //Light has a probability of arriving to the point and some light does
+	if (light_pdf > 0.0f && Li != glm::vec3(0.0f)) //Light has a probability of arriving at the point and some light does
 	{
-		glm::vec3 f = isect.bsdf->f(isect.ray->dir, wi, BxDFType(BSDF_ALL & ~BSDF_SPECULAR));
+		glm::vec3 f = isect.bsdf->f(-isect.ray->dir, wi, BxDFType(BSDF_ALL & ~BSDF_SPECULAR));
 		f *= glm::abs(glm::dot(isect.normal, wi));
-		//LOG_MESSAGE(true, "F: %f %f %f\n", f.x, f.y, f.z);
 		if (f != glm::vec3(0.0f)) //Some light is reflected from the intersection point
 		{
 			//Check for visibility to light
 			Ray vis_ray(isect.point, wi);
 			SurfaceInteraction light_isect;
-			//HACK: This should probably be dealt with in a better way
-			//The subtraction of 0.0001f make it so that an intersection with the light itself isn't detected
-			if (!scene.Intersect(&vis_ray, &light_isect, glm::length(vis_ray.origin - sample_point) - 0.0001f))
-			{
+			if (!scene.Intersect(&vis_ray, &light_isect, 0.0001f, 10000.0f) || light_isect.shape == area_light->shape)
+			{		
 				const float weight = 1.0f; //TODO: PowerHeuristic
 				Ld += f * Li * weight / light_pdf;
-				//LOG_MESSAGE(true, "Ld: %f %f %f\n", Ld.x, Ld.y, Ld.z);
 			}
 		}
 	}
 	
 	//Sample BRDF of intersection point
-	/*BxDFType sampled_type;
+	BxDFType sampled_type;
 	float scattering_pdf = 0.0f;
-	glm::vec3 f = isect.bsdf->Samplef(rng, isect.ray->dir, u_scattering, BxDFType(BSDF_ALL & ~BSDF_SPECULAR), isect.normal, &wi, &scattering_pdf, &sampled_type);
+	glm::vec3 f = isect.bsdf->Samplef(rng, -isect.ray->dir, u_scattering, BxDFType(BSDF_ALL & ~BSDF_SPECULAR), isect.normal, &wi, &scattering_pdf, &sampled_type);
 	f *= glm::abs(glm::dot(isect.normal, wi));
 	if (f != glm::vec3(0.0f) && scattering_pdf > 0.0f)
 	{
@@ -91,7 +85,7 @@ glm::vec3 EstimateDirect(const Scene& scene, RNG& rng, const AreaLight* area_lig
 		{
 			Ld += f * Li * weight / scattering_pdf;
 		}
-	}*/
+	}
 
 	return Ld;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char** argv)
 	//Init stuff
 	RNG rngs[omp_get_max_threads()];
 	const int width = 720, height = 480;
-	const int ssp = 20;
+	const int ssp = 512;
 	float* image = new float[width * height * 3];
 
 	auto start = GetTime();
@@ -27,7 +27,7 @@ int main(int argc, char** argv)
 	start = GetTime();
 	for (int y = 0; y < height; y++)
 	{
-//#pragma omp parallel for
+#pragma omp parallel for
 		for (int x = 0; x < width; x++)
 		{
 			float u = float(x) / float(width);
@@ -44,22 +44,8 @@ int main(int argc, char** argv)
 					{
 						L += scene->area_lights[isect.shape->area_light_index]->L(isect.point, -isect.ray->dir);
 					}
-					else //TODO: rmeove when done debugging
-					{
-						L += UniformSampleOne(*scene, isect, rngs[omp_get_thread_num()]);
-						if (L == glm::vec3(0.0f))
-						{
-							//Problem: a pixel will get either all OK or OK2, which results in some pixels being completely black
-							//Why does this happen??
-							//LOG_MESSAGE(false, "OK\n");
-						}
-						else
-						{
-							//LOG_MESSAGE(false, "OK2\n");
-						}
-					}
+					L += UniformSampleOne(*scene, isect, rngs[omp_get_thread_num()]);
 				}
-				//LOG_MESSAGE(true, "--------------\n");
 			}
 			L /= float(ssp);
 			
